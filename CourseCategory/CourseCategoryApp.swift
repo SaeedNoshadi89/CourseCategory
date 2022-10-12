e611680f9ce9fe858cb074e1eb5640e17e713960
Saeed Noshadi <saeednoshadi89@gmail.com>
2022-10-12 22:31:01 +0330

    Initial Commit

---
+++ CourseCategory/CourseCategoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseCategoryApp.swift
+//  CourseCategory
+//
+//  Created by $@€€© on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CourseCategoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
